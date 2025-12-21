e9862928113a320282be1fba1478aa35e6c4c575
janphilippgutt <janphilippgutt@posteo.de>
2025-12-21 09:13:38 +0100

    Enhance logging middleware to include HTTP status code

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -6,19 +6,35 @@ import (
 	"time"
 )
 
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.statusCode = code                // record status code
+	r.ResponseWriter.WriteHeader(code) // call the real writer
+}
+
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		recorder := &statusRecorder{
+			ResponseWriter: w,
+			statusCode:     200, // default to 200 OK: If the handler never calls WriteHeader, Go assumes 200
+		}
+
 		// Call the next handler
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(recorder, r)
 
 		duration := time.Since(start)
 
 		fmt.Printf(
-			"method=%s path=%s duration=%s\n",
+			"method=%s path=%s status=%d duration=%s\n",
 			r.Method,
 			r.URL.Path,
+			recorder.statusCode,
 			duration,
 		)
 	})
